e817b1cdc0750f937d912888c1623f60f647b112
Valeriy Seregin <valeriy@seregin.io>
2020-03-25 13:30:09 +0300

    change preprocessValue function

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,15 +1,12 @@
-import _ from 'lodash';
-
-const isComplex = (value) => {
-  if (_.isObject(value)) {
-    return '[complex value]';
+const preprocessValue = (value) => {
+  switch (typeof value) {
+    case ('object'):
+      return '[complex value]';
+    case ('string'):
+      return `'${value}'`;
+    default:
+      return value;
   }
-
-  if (_.isString(value)) {
-    return `'${value}'`;
-  }
-
-  return value;
 };
 
 const generatePath = (parents, currentKey) => [...parents, currentKey].join('.');
@@ -18,16 +15,19 @@ const mappings = {
   nested: (node, parentKeys, iter) => iter(node.children, [...parentKeys, node.key]),
   unchanged: () => '',
   changed: (node, parentKeys) => {
-    const oldVal = isComplex(node.oldValue);
-    const newVal = isComplex(node.newValue);
+    const oldVal = preprocessValue(node.oldValue);
+    const newVal = preprocessValue(node.newValue);
     return `Property '${generatePath(parentKeys, node.key)}' was changed from ${oldVal} to ${newVal}`;
   },
-  added: (node, parentKeys) => `Property '${generatePath(parentKeys, node.key)}' was added with value: ${isComplex(node.value)}`,
+  added: (node, parentKeys) => `Property '${generatePath(parentKeys, node.key)}' was added with value: ${preprocessValue(node.value)}`,
   deleted: (node, parentKeys) => `Property '${generatePath(parentKeys, node.key)}' was deleted`,
 };
 
 const iter = (ast, parentKeys) => ast.map((node) => mappings[node.state](node, parentKeys, iter));
 
-const render = (ast) => _.flattenDeep(iter(ast, [])).filter((str) => str).join('\n');
+const render = (ast) => iter(ast, [])
+  .flat(Infinity)
+  .filter((str) => str)
+  .join('\n');
 
 export default render;
