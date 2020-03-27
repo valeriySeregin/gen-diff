7befa3deb5ef0c755154a501ca91d09b32c69722
Valeriy Seregin <valeriy@seregin.io>
2020-03-27 15:48:30 +0300

    create getContent function

--- src/index.js
+++ src/index.js
@@ -2,24 +2,21 @@ import path from 'path';
 import fs from 'fs';
 import buildAst from './ast';
 import parse from './parsers';
-import render from './formatters';
+import format from './formatters';
 
 const readFile = (filepath) => fs.readFileSync(filepath, 'utf-8');
 
-export default (firstConfig, secondConfig, format) => {
-  const data1 = readFile(firstConfig);
-  const data2 = readFile(secondConfig);
+const getContent = (config) => {
+  const data = readFile(config);
+  const dataType = path.extname(config).slice(1);
 
-  const dataTypeBefore = path.extname(firstConfig).slice(1);
-  const dataTypeAfter = path.extname(secondConfig).slice(1);
+  return parse(data, dataType);
+};
 
-  const contentBefore = parse(data1, dataTypeBefore);
-  const contentAfter = parse(data2, dataTypeAfter);
-
+export default (firstConfig, secondConfig, formatName) => {
+  const contentBefore = getContent(firstConfig);
+  const contentAfter = getContent(secondConfig);
   const ast = buildAst(contentBefore, contentAfter);
 
-  const getRenderedTree = render(format);
-  const diff = getRenderedTree(ast);
-
-  return diff;
+  return format(formatName, ast);
 };
