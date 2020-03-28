482e2c95a18f3bcbdb3116f89971e1a53f624730
Valeriy Seregin <valeriy@seregin.io>
2020-03-28 11:23:33 +0300

    fix tree.js

--- src/formatters/tree.js
+++ src/formatters/tree.js
@@ -9,14 +9,13 @@ const stringify = (value, depth) => (
 );
 
 const mappings = {
-  nested: ({ key, children }, depth, iter) => {
-    const elements = iter(children, depth + 1).join('\n');
-    return `${indent(depth)}    ${key}: {\n${elements}\n${indent(depth + 1)}}`;
-  },
+  nested: ({ key, children }, depth, iter) => `${indent(depth)}    ${key}: {\n${iter(children, depth + 1).join('\n')}\n${indent(depth + 1)}}`,
   unchanged: ({ key, value }, depth) => `${indent(depth)}    ${key}: ${stringify(value, depth)}`,
-  changed: ({ key, oldValue, newValue }, depth) => (
-    `${indent(depth)}  - ${key}: ${stringify(oldValue, depth)}\n${indent(depth)}  + ${key}: ${stringify(newValue, depth)}`
-  ),
+  changed: ({ key, oldValue, newValue }, depth) => {
+    const oldValueString = `${indent(depth)}  - ${key}: ${stringify(oldValue, depth)}`;
+    const newValueString = `${indent(depth)}  + ${key}: ${stringify(newValue, depth)}`;
+    return `${oldValueString}\n${newValueString}`;
+  },
   added: ({ key, value }, depth) => `${indent(depth)}  + ${key}: ${stringify(value, depth)}`,
   deleted: ({ key, value }, depth) => `${indent(depth)}  - ${key}: ${stringify(value, depth)}`,
 };
